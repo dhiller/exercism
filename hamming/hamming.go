6c266bf867e4cc583460bd8d72b4b193fe7d2ebe
Daniel Hiller <daniel.hiller.1972@googlemail.com>
2019-08-25 10:20:59 +0200

    Name return parameters

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -5,11 +5,10 @@ import (
 )
 
 // Distance calculates the Hamming distance between two strings.
-func Distance(a, b string) (int, error) {
+func Distance(a, b string) (hamming int, err error) {
 	if len(a) != len(b) {
-		return 0, fmt.Errorf("a and b have different length (%d != %d)", len(a), len(b))
+		return hamming, fmt.Errorf("a and b have different length (%d != %d)", len(a), len(b))
 	}
-	hamming := 0
 	runesA := []rune(a)
 	runesB := []rune(b)
 	for index, runeFromA := range runesA {
